5cdde5dd406478bf75a2847a6a4296e3b6f8d645
nebunet <109931844+nebunet@users.noreply.github.com>
2022-12-19 20:22:58 -0500

    Update mango.lua

--- mango.lua
+++ mango.lua
@@ -3,27 +3,17 @@ local reanim = game.Players.LocalPlayer.Character
 local uis = game:GetService("UserInputService")
 local srv = game:GetService("RunService")
 
-local renderstepped = srv.RenderStepped
 local hb = srv.Heartbeat
 
 local m = math
 local c = CFrame
 local cf = c.new
 local angles = c.Angles
-local v3 = Vector3.new
 local rad = m.rad
-local sin = m.sin
-local cos = m.cos
-local abs = m.abs
 
 local plr = game:GetService("Players").LocalPlayer
 local humanoid = reanim:FindFirstChild("Humanoid")
 
-local la = reanim["Left Arm"]
-local ra = reanim["Right Arm"]
-local ll = reanim["Left Leg"]
-local rl = reanim["Right Leg"]
-local head = reanim["Head"]
 local torso = reanim["Torso"]
 local root = reanim["HumanoidRootPart"]
 
@@ -41,62 +31,14 @@ local lhC0 = torso["Left Hip"].C0
 local nC0 = torso["Neck"].C0
 local rC0 = root["RootJoint"].C0
 
-local function rootLerp(lerps, dt)
-    r.C0 = r.C0:Lerp(rC0 * cf(lerps), dt)
-end
-
-local function neckLerp(lerps, dt)
-    n.C0 = n.C0:Lerp(nC0 * cf(lerps), dt)
-end
-
-local function rightArmLerp(lerps, dt)
-    rs.C0 = rs.C0:Lerp(rsC0 * cf(lerps), dt)
-end
-
-local function leftArmLerp(lerps, dt)
-    ls.C0 = ls.C0:Lerp(lsC0 * cf(lerps), dt)
-end
-
-local function rightLegLerp(lerps, dt)
-    rh.C0 = rh.C0:Lerp(rhC0 * cf(lerps), dt)
-end
-
-local function leftLegLerp(lerps, dt)
-    lh.C0 = lh.C0:Lerp(lhC0 * cf(lerps), dt)
-end
-
-local RL = cf(0, 0, 0) * angles(0, 0, 0)
-local HL = cf(0, 0, 0) * angles(0, 0, 0)
-local RAL = cf(0, 0, 0) * angles(0, 0, 0)
-local LAL = cf(0, 0, 0) * angles(0, 0, 0)
-local RLL = cf(0, 0, 0) * angles(0, 0, 0)
-local LLL = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().RootLerps = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().NeckLerps = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().RightShoulderLerps = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().LeftShoulderLerps = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().RightHipLerps = cf(0, 0, 0) * angles(0, 0, 0)
+getgenv().LeftHipLerps = cf(0, 0, 0) * angles(0, 0, 0)
 
 local mango = {}
-
-function mango:SetHeadLerps(lerps)
-    HL = lerps
-end
-
-function mango:SetRootLerps(lerps)
-    RL = lerps
-end
-
-function mango:SetRightArmLerps(lerps)
-    RAL = lerps
-end
-
-function mango:SetLeftArmLerps(lerps)
-    LAL = lerps
-end
-
-function mango:SetRightLegLerps(lerps)
-    RLL = lerps
-end
-
-function mango:SetLeftLegLerps(lerps)
-    LLL = lerps
-end
 
 function mango:SetJumpPower(jp)
     humanoid.JumpPower = jp
@@ -117,12 +59,12 @@ function mango:Initiate()
         sine = sine + dt
         dt = dt * 10
 
-        rootLerp(RL, dt)
-        neckLerp(HL, dt)
-        rightArmLerp(RAL, dt)
-        leftArmLerp(LAL, dt)
-        rightLegLerp(RLL, dt)
-        leftLegLerp(LLL, dt)
+        r.C0 = r.C0:Lerp(rC0 * cf(RootLerps), dt)
+        n.C0 = n.C0:Lerp(nC0 * cf(NeckLerps), dt)
+        rs.C0 = rs.C0:Lerp(rsC0 * cf(RightShoulderLerps), dt)
+        ls.C0 = ls.C0:Lerp(lsC0 * cf(LeftShoulderLerps), dt)
+        rh.C0 = rh.C0:Lerp(rhC0 * cf(RightHipLerps), dt)
+        lh.C0 = lh.C0:Lerp(lhC0 * cf(LeftHipLerps), dt)
     end)
 end
 
